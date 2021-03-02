afeb907d87b01b8d5b09290d38bad254c21f4119
margaritavas <76732128+margaritavas@users.noreply.github.com>
2021-03-02 22:14:20 +0100

    Add files via upload

--- dApp/main.js
+++ dApp/main.js
@@ -1,10 +1,10 @@
 var web3 = new Web3(Web3.givenProvider);
 var contractInstance;
-var selection;
+var coinSelection;
 
 $(document).ready(function() {
     window.ethereum.enable().then(function(accounts){
-      contractInstance = new web3.eth.Contract(abi,"0x6182729cb01434A7FEf4a9b35c70acC276466f2F", {from: accounts[0]});
+      contractInstance = new web3.eth.Contract(abi,"0x1772AF08884b6Ad9AcdA83F288D75f0d2Cc418a6", {from: accounts[0]});
       console.log(contractInstance);
     });
 
@@ -17,20 +17,20 @@ $("#confirm_button").click(flipCoin);
 });
 
 function heads(){
-  selection=1;
-  alert("You have selected Head!")
+  coinSelection=1;
+  $("#selection").text("Your selection is: Heads");
 }
 
 function tails(){
-  selection=0;
-  alert("You have selected Tails!")
+  coinSelection=0;
+  $("#selection").text("Your selection is: Tails");
 }
 
 function flipCoin(selection){
   var bet = $("#bet_input").val();
       bet= web3.utils.toWei(bet, "Ether")
 
-contractInstance.methods.flipCoin(selection).send({value:bet})
+contractInstance.methods.flipCoin(coinSelection).send({value:bet})
 .on("transactionHash", function(hash){
   console.log(hash);
 })
@@ -38,11 +38,11 @@ contractInstance.methods.flipCoin(selection).send({value:bet})
   console.log(confirmationNr);
 })
 .on("receipt", function(receipt){
-          if(receipt.events.Result.returnValues.result == false){
-            alert("You lost!");
+          if (receipt.events.result.returnValues.betResult == true){
+            alert("You won! You doubled your bet!");
           }
-          else if(receipt.events.Result.returnValues.result == true){
-            alert("You won!");
+          else if (receipt.events.result.returnValues.betResult == false){
+            alert("You lost! Better luck next time!");
           }
       });
     };
