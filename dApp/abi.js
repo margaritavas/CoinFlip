e1e057dcd38e5628e80861497ce5b4e3834cd5a5
margaritavas <76732128+margaritavas@users.noreply.github.com>
2021-03-02 02:49:29 +0100

    Add files via upload

---
+++ dApp/abi.js
@@ -0,0 +1,138 @@
+abi =[
+  {
+    "inputs": [],
+    "payable": true,
+    "stateMutability": "payable",
+    "type": "constructor"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": false,
+        "internalType": "address",
+        "name": "player",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "internalType": "bool",
+        "name": "betResult",
+        "type": "bool"
+      },
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "valueBack",
+        "type": "uint256"
+      }
+    ],
+    "name": "Result",
+    "type": "event"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "balance",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "owner",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [],
+    "name": "addBalance",
+    "outputs": [],
+    "payable": true,
+    "stateMutability": "payable",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "random",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "coinGuess",
+        "type": "uint256"
+      }
+    ],
+    "name": "flipCoin",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "betResult",
+        "type": "bool"
+      }
+    ],
+    "payable": true,
+    "stateMutability": "payable",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "getBalance",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [],
+    "name": "withdrawAll",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }
+]
---
+++ dApp/main.js
@@ -0,0 +1,47 @@
+var web3 = new Web3(Web3.givenProvider);
+var contractInstance;
+var selection;
+
+$(document).ready(function() {
+    window.ethereum.enable().then(function(accounts){
+      contractInstance = new web3.eth.Contract(window.abi,"0x73A70c912538c614978Fa0cF6929890B622C9bD9", {from: accounts[0]});
+      console.log(contractInstance);
+    });
+
+$("#head_button").click(heads);
+
+$("#tails_button").click(tails);
+
+$("#confirm_button").click(flipCoin);
+
+});
+
+function heads(){
+  selection=1;
+  alert("You have selected Head!")
+}
+
+function tails(){
+  selection=0;
+  alert("You have selected Tails!")
+}
+
+function flipCoin(){
+  var bet = $("#bet_input").val();
+      bet= web3.utils.toWei(bet, "Ether")}
+
+contractInstance.methods.flipCoin(selection).send({value:bet})
+.on("transactionHash", function(hash){
+  console.log(hash);
+})
+.on("confirmationNr", function(confirmationNr){
+  console.log(confirmationNr);
+})
+.on("receipt", function(receipt){
+          if(receipt.events.Result.returnValues.result == false){
+            alert("You lost!");
+          }
+          else if(receipt.events.Result.returnValues.result == true){
+            alert("You won!");
+          }
+      })
