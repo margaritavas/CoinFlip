050c972809138849a54bc83ccfd24bf98ef1a5b2
margaritavas <76732128+margaritavas@users.noreply.github.com>
2021-03-12 01:00:25 +0100

    Add files via upload

--- dApp/main.js
+++ dApp/main.js
@@ -5,7 +5,7 @@ var playerAddress;
 
 $(document).ready(function() {
     window.ethereum.enable().then(function(accounts){
-      contractInstance = new web3.eth.Contract(abi,"0x29337fb1FE9cB5499BC50ef7B80431f10ed0db2d", {from: accounts[0]});
+      contractInstance = new web3.eth.Contract(window.abi,"0x29337fb1FE9cB5499BC50ef7B80431f10ed0db2d", {from: accounts[0]});
       console.log(contractInstance);
     });
 
@@ -44,26 +44,30 @@ contractInstance.methods.coinFlip(coinSelection).send(config)
 .on("receipt", function(receipt){
   console.log(receipt);
   playerAddress = receipt.from;
-  console.log("Player address is" + receipt.from);
+  console.log("This is the players address: " + receipt.from);
 })
 
-contractInstance.events.generatedRandomNumber(function(error, event){
-          console.log(event.returnValues);
-          console.log("Generated random number for player "+ playerAddress + " is : " + event.returnValues.randomNumber);
-        })
+contractInstance.once('generatedRandomNumber', {
+            filter: {address: playerAddress},
+            fromBlock:'latest'
+          }, function(error, event){
+            console.log(event);
+            console.log("The random number generated for the player "+playerAddress+" is : "+event.returnValues.randomNumber);
+          });
 
-contractInstance.events.result(function(error, event){
-                    console.log(event.returnValues);
+contractInstance.once('result', {
+            filter: {address: playerAddress},
+            fromBlock:'latest'
+          }, function(error, event){
+           console.log(event);
 
-          if(event.returnValues.betResult == true) {
-            console.log("The bet result is:" + event.returnValues.betResult);
-             alert("You won! You doubled your bet!");
-      }
-          else {
-            console.log("The bet result is:" + event.returnValues.betResult);
-              alert("You lost! Better luck next time!");
-          }
+           if (event.returnValues.betResult == true) {
+             console.log("Bet result is: "+event.returnValues.betResult)
+             alert("Congratulations! You won!");
+           }else {
+             console.log("Bet result is: "+event.returnValues.betResult)
+                   alert("You lost! More luck next time!");
+             };
+           });
 
-      })
-
-    };
+};
